888c8cde7dd0fd2cd97562d398a9776ba831d75e
joaoalflopes <68ronins@gmail.com>
2020-08-21 15:34:25 -0300

    Projeto criado

---
+++ src/main/java/com/jboyCorp/webserviceRestFul/WebserviceRestFulApplication.java
@@ -0,0 +1,13 @@
+package com.jboyCorp.webserviceRestFul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceRestFulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceRestFulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jboyCorp/webserviceRestFul/WebserviceRestFulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jboyCorp.webserviceRestFul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceRestFulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
