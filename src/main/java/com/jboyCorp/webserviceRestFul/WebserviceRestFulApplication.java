e68a4177a4e38dca5ff4730b557bb9b4473ac2b1
joaoalflopes <68ronins@gmail.com>
2020-08-22 00:37:54 -0300

    Created entity product and implematation of the association many-to-many

--- src/main/java/com/jboyCorp/webserviceRestFul/WebserviceRestFulApplication.java
+++ src/main/java/com/jboyCorp/webserviceRestFul/WebserviceRestFulApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.jboyCorp.webserviceRestFul.domain.Category;
+import com.jboyCorp.webserviceRestFul.domain.Product;
 import com.jboyCorp.webserviceRestFul.repositories.CategoryRepository;
+import com.jboyCorp.webserviceRestFul.repositories.ProductRepository;
 
 @SpringBootApplication
 public class WebserviceRestFulApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	@Autowired
+	private ProductRepository productRepository;
 	
 
 	public static void main(String[] args) {
@@ -35,7 +39,28 @@ public class WebserviceRestFulApplication implements CommandLineRunner {
 		Category cat9 = new Category(null, "Home & Kitchen");
 		Category cat10 = new Category(null, "Automotive");
 		
+		Product p1 = new Product(null, "Jelly Comb 2.4G Slim Wireless Mouse with Nano Receiver", 11.99);
+		Product p2 = new Product(null, "Mpow HC6 USB Headset with Microphone, Comfort-fit Office Computer Headphone", 36.90);
+		Product p3 = new Product(null, "Samsung Galaxy Buds+ Plus, True Wireless Earbuds (Wireless Charging Case included)", 149.99);
+		Product p4 = new Product(null, "Car Vacuum, HOTOR Corded Car Vacuum Cleaner High Power for Quick Car Cleaning, DC 12V ", 25.49);
+		Product p5 = new Product(null, "ZippyPaws - Skinny Peltz No Stuffing Squeaky Plush Dog Toy", 9.80);
+		Product p6 = new Product(null, "Kaspersky Internet Security 2020,3 Devices,1 Year,PC/Mac/Android ", 36.99);
+		
+		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
+		cat4.getProducts().addAll(Arrays.asList(p2, p3));
+		cat10.getProducts().add(p4);
+		cat7.getProducts().add(p5);
+		cat5.getProducts().add(p6);
+		
+		p1.getCategories().add(cat1);
+		p2.getCategories().addAll(Arrays.asList(cat1, cat4));
+		p3.getCategories().addAll(Arrays.asList(cat1, cat4));
+		p4.getCategories().add(cat10);
+		p5.getCategories().add(cat7);
+		p6.getCategories().add(cat5);
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6));
 		
 	}
 
--- src/main/java/com/jboyCorp/webserviceRestFul/domain/Product.java
+++ src/main/java/com/jboyCorp/webserviceRestFul/domain/Product.java
@@ -8,32 +8,41 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class Category implements Serializable {
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	private Double price;
 	
-	@ManyToMany(mappedBy = "categories")
-	private List<Product> products = new ArrayList<>();
-			
-	public Category() {
+	@JsonIgnore
+	@ManyToMany
+	@JoinTable(name = "PRODUCT_CATEGORY",
+	joinColumns = @JoinColumn (name = "produtc_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id")
+	)
+	private List<Category> categories = new ArrayList<>();
+	
+		
+	public Product() {
 	}
 
-	public Category(Integer id, String name) {
+	public Product(Integer id, String name, double price) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.price = price;
 	}
-
+		
 	public Integer getId() {
 		return id;
 	}
@@ -49,13 +58,21 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public List<Product> getProducts() {
-		return products;
+
+	public Double getPrice() {
+		return price;
 	}
 
-	public void setProducts(List<Product> products) {
-		this.products = products;
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
 	}
 
 	@Override
@@ -74,7 +91,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
